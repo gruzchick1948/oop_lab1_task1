50e90cb9bead8a57c94cdf37cfc3549b80538dde
Dr.Gruzchik <90223414+gruzchick1948@users.noreply.github.com>
2021-10-14 13:00:03 +0300

    Add files via upload

---
+++ Lab_03/Lab_03/Class1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_03
+{
+    partial class phone
+    {
+        static int id;
+        private string name;
+        private string surname;
+        private string patron;
+        static int addr;
+        private int number;
+        private int cred;
+        private int deb;
+        private int intown, outown;
+
+        public phone(string n, string s, string p, int numb, int cred, int deb, int intt, int outt)
+        {
+            this.name = n;
+            this.surname = s;
+            this.patron = p;
+            this.number = numb;
+            this.cred = cred;
+            this.deb = deb;
+            this.intown = intt;
+            this.outown = outt;
+        }
+        // get and set
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public string Name
+        {
+            get{ return name; }
+            set{ name = value; }
+        }
+        public string Surname
+        {
+            get{ return surname; }
+            set{ surname = value; }
+        }
+        public string Patron
+        {
+            get
+            {
+                return patron;
+            }
+            set
+            {
+                patron = value;
+            }
+        }
+        public int Addr
+        {
+            get
+            {
+                return addr;
+            }
+        }
+        public int Number
+        {
+            get
+            { return number; }
+            set
+            {
+                if (value > 0){ number = value; }
+                else
+                {
+                    Console.WriteLine("Неверный формат!");
+                }
+            }
+        }
+        public int Deb
+        {
+            get
+            {
+                return deb;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    deb = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат!");
+                }
+            }
+        }
+        public int Cred
+        {
+            get
+            {
+                return cred;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    cred = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат!");
+                }
+            }
+        }
+        public int Intown
+        {
+            get
+            {
+                return intown;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    intown = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат!");
+                }
+            }
+        }
+        public int Outtown
+        {
+            get
+            {
+                return outown;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    outown = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат!");
+                }
+            } 
+        }
+        public void balance(phone phone1)
+        {
+            Console.WriteLine($"Баланс номера {phone1.number}: {phone1.deb - phone1.cred}");
+        }
+
+
+        //поля
+        private readonly int ID;
+        private const int MAX_ID = 99;
+
+
+        //конструкторы
+        public phone()
+        {
+            ID = GetHashCode();
+        }
+        public phone(string name, int number =1245,  string surname = "Раченок")
+        {
+            if (!string.IsNullOrEmpty(name) && id <= MAX_ID)
+            {
+                this.name = name;
+                this.number = number;
+                this.surname = surname;
+                id++;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод!");
+            }
+        }
+        static phone()
+        {
+            addr = 267;
+        }
+
+        //// закрытый конструктор
+        //private phone() { }
+
+
+        public void Changeaddr(ref int newaddr, out int addr)
+        {
+            addr = newaddr;
+            Console.WriteLine("Новаый адрес: " + addr);
+        }
+
+        public static void PrintInfo()
+        {
+            Console.WriteLine($"\nИнформация о классе:\nИмя класса: phone\nКоличество объектов: {id}\nМаксимальное количество: {MAX_ID}");
+        }
+    }
+    partial class phone
+    {
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+            {
+                Console.WriteLine("Объект null");
+            }
+            phone phone2 = obj as phone;
+            return phone2.Name == Name && phone2.Number == Number && phone2.Surname == Surname;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 269;
+            hash = string.IsNullOrEmpty(Name) ? 0 : Name.GetHashCode();
+            hash = (hash * 47) + number.GetHashCode();
+            return hash;
+
+        }
+
+        public override string ToString()
+        {
+            return $"\nИмя: {Name}; Фамилия: {Surname}; Номер: {Number}; Адрес: {addr}";
+        }
+    }
+}
---
+++ Lab_03/Lab_03/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Lab_03
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                phone[] phone1 = new phone[6];
+                phone1[0] = new phone("Илья", "Раченок", "Александрович", 2076, 200, 300, 4, 0);
+                phone1[1] = new phone("Андрей", "Бердяев", "Максимович", 3215, 523, 98, 5, 0);
+                phone1[2] = new phone("Александр", "Хлебников", "Васильевич", 4015, 768, 89, 5, 4);
+                phone1[3] = new phone("Глеб", "Крученых", "Александрович", 1235, 100, 456, 12, 8);
+                phone1[4] = new phone("Владислав", "Терентьев", "Андреевич", 1189, 267, 300, 11, 8);
+                phone1[5] = new phone("Софья", "Бурлюк", "Сергеевна", 4328, 678, 454, 5, 5);
+
+                Console.WriteLine("сведения об абонентах, у которых время внутригородских разговоров превышает 10:");
+                for (int i = 0; i < 6; i++)
+                {
+                    if (phone1[i].Intown > 10)
+                        Console.WriteLine($"ФИО:{phone1[i].Name} {phone1[i].Surname} {phone1[i].Patron}; Номер: {phone1[i].Number};");
+                }
+                Console.WriteLine("\n\nсведения об абонентах, которые пользовались междугородной связью:");
+                for (int i = 0; i < 6; i++)
+                {
+                    if (phone1[i].Outtown > 0)
+                        Console.WriteLine($"ФИО:{phone1[i].Name} {phone1[i].Surname} {phone1[i].Patron}; Номер: {phone1[i].Number};");
+                }
+                Console.WriteLine("\n\nБалансы номеров:");
+                for (int i = 0; i < 6; i++)
+                {
+                    phone1[i].balance(phone1[i]);
+                }
+
+                phone gleb1 = new phone("Глеб");
+                phone gleb = new phone("Глеб", 3469, "Пташков");
+                phone vlad = new phone("Владислав", 1232, "Папко");
+                phone vlad2 = new phone("Владислав", 1232, "Папко");
+                phone maks = new phone("Максим", 1039, "Васильев");
+
+                Console.WriteLine(gleb.ToString());
+                Console.WriteLine("Сравнение объектов(true or false):");
+                Console.WriteLine(vlad.Equals(vlad2));
+                Console.WriteLine(maks.GetHashCode());
+                int num = 1423, newnum;
+                gleb.Changeaddr(ref num, out newnum);
+
+
+                phone.PrintInfo();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
